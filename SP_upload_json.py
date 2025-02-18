73e2757df39d934cec25102ad7e9cb22d07bc012
mehmeterengunduz6 <mehmeterengunduz6@gmail.com>
2025-02-18 20:03:50 +0300

    new

--- SP_upload_json.py
+++ SP_upload_json.py
@@ -5,10 +5,13 @@ from decimal import Decimal
 import django
 
 # Set the Django settings module environment variable
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "afa_fund.settings")  # Use your actual settings module path
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "afa_fund.settings"
+)  # Use your actual settings module path
 django.setup()
 
 from core.models import Stock  # Adjust this import based on your app's name
+
 
 def SP_upload_json(file_path, date_value):
     # Open the JSON file
@@ -18,15 +21,16 @@ def SP_upload_json(file_path, date_value):
     # Process each record and save to database
     for item in data:
         stock = Stock(
-            symbol=item['symbol'],
-            name=item['name'],
-            weight=round(Decimal(item['weight']) * 100, 2),  # Convert weight to Decimal
-            date=date_value, # Use the date passed as input
-            is_AFA=False
+            symbol=item["symbol"],
+            name=item["name"],
+            weight=Decimal(item["weight"]),  # Convert weight to Decimal
+            date=date_value,  # Use the date passed as input
+            is_AFA=False,
         )
         stock.save()
 
     print(f"Uploaded {len(data)} records successfully.")
+
 
 if __name__ == "__main__":
     # Get file path and date from the command line arguments
@@ -38,5 +42,5 @@ if __name__ == "__main__":
     date_value = sys.argv[2]  # Second argument is the date (e.g., '2024-09-01')
 
     SP_upload_json(file_path, date_value)
-    
+
 # python SP_upload_json.py /path/to/your/json_file.json 2024-09-01
